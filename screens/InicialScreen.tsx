1d6766c415f8d4967268d824cfe03c59bbc559e3
Felipe Galvão <canallinho@gmail.com>
2020-09-01 00:17:31 -0300

    Modificação na tela de Splash e Produtos na Tela Inicial

--- screens/InicialScreen.tsx
+++ screens/InicialScreen.tsx
@@ -1,10 +1,58 @@
 import * as React from "react";
 import { Text, View } from "../components/Themed";
+import { Image, StyleSheet, ActivityIndicator } from "react-native";
+import { ScrollView, FlatList } from "react-native-gesture-handler";
 
 export default function Inicial() {
+  const [carregado, setCarregado] = React.useState(true);
+  const [dados, setDados] = React.useState([]);
+
+  // Carregar a api com os dados do banco de dados
+  // Executar a consulta listartelainicial
+
+  React.useEffect(() => {
+    fetch(
+      "http://192.168.0.23/projeto-app-loja/service/produto/listartelainicial.php"
+    )
+      // Então, vamos transformar a resposta em JSON
+      .then((response) => response.json())
+      // Então, vamos dizer que tudo o que está vindo são produtos e colocar eles em setDados
+      .then((produtos) => setDados(produtos.saida))
+      .catch((error) => console.error(error))
+      // Finalizando interrompendo o carregamento
+      .finally(() => setCarregado(false));
+  }, []);
+
   return (
     <View>
-      <Text> Estamos dentro da tela inicial </Text>
+      <ScrollView>
+        <Image
+          source={require("../assets/images/produtos.png")}
+          style={estilos.imagem}
+        />
+        {carregado ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+            data={dados}
+            renderItem={({ item }) => (
+              <View>
+                <Image source={require("../assets/images/mousepad1.png")} />
+                <Text>
+                  {item.nomeproduto} | {item.preco}
+                </Text>
+              </View>
+            )}
+            keyExtractor={({ idproduto }, index) => idproduto}
+          />
+        )}
+      </ScrollView>
     </View>
   );
 }
+
+const estilos = StyleSheet.create({
+  imagem: {
+    width: "100%",
+  },
+});
